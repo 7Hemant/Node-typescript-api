4900fd459832eb1a62be816796e5333d31ae4db0
=hemant <=hemantmourya127@gmail.com>
2024-06-22 00:59:29 +0530

    server setup

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,13 @@
-console.log("welcome to node-typescript-api project")
+import express,{ Request, Response } from "express";
+
+const app = express();
+
+const PORT = 3000 || process.env.PORT
+
+app.use("/",(req:Request,res:Response)=>{
+ res.send("this is node-typescipt-api")
+})
+
+app.listen(PORT,()=>{
+    console.log("node-typescipt-api server is live on PORT:",PORT)
+})
