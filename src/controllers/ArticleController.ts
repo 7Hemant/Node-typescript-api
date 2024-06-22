7af3dc9e475c7eadde9d6f434e6fac0a3f0e5f66
=hemant <=hemantmourya127@gmail.com>
2024-06-22 23:27:50 +0530

    fixes

--- src/controllers/ArticleController.ts
+++ src/controllers/ArticleController.ts
@@ -18,7 +18,7 @@ export const  deleteArticle = async(req:Request,res:Response)=>{
     const ArticleId = req.params.id;
     const Articlefinded = await prisma.article.findUnique({where:{ id:Number(ArticleId)}})
     
-    if(!Articlefinded) res.json({"message":"not found"}); return;
+    if(!Articlefinded) {res.json({"message":"not found"}); return;}
 
     const deleteArticle = await prisma.article.delete({
         where:{
@@ -31,7 +31,7 @@ export const  updateArticle = async(req:Request,res:Response)=>{
     const ArticleId = req.params.id;
     const Articlefinded = await prisma.article.findUnique({where:{ id:Number(ArticleId)}})
     
-    if(!Articlefinded) res.json({"message":"not found"}); return;
+    if(!Articlefinded) {res.json({"message":"not found"}); return;}
 
     const updatedArticle = await prisma.article.update({
         where:{
